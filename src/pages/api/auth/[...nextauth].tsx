a2a0a2ab88d5293b333de903a15eca8f42b0f89c
vinisp <aragao93.vinicius@gmail.com>
2022-05-30 14:57:13 -0300

    corrigir dois

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
