2b2766517c5d056b1d8c25043ca6db9f77f633af
vinisp <aragao93.vinicius@gmail.com>
2022-06-28 16:00:12 -0300

    testando email

--- src/pages/api/sub/[sid].ts
+++ src/pages/api/sub/[sid].ts
@@ -14,29 +14,29 @@ export default async function handler(
   const { data: session } = useSession()
   const userEmail = session?.user?.email as string
 
-  if (req.method === 'POST') {
-    try {
-      // Create Checkout Sessions from body params.
-      const sessionStripe = await stripe.checkout.sessions.create({
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: sid as string,
-            quantity: 1
-          }
-        ],
-        mode: 'subscription',
-        customer_email: userEmail,
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`
-      })
-      return res.redirect(303, sessionStripe.url as string)
-    } catch (err) {
-      return res.send({ message: 'error 1' })
-    }
-  } else {
-    res.setHeader('Allow', 'POST')
-    res.status(405).end('Method Not Allowed')
-  }
-  return res.send({ message: 'error 2' })
+  //   if (req.method === 'POST') {
+  //     try {
+  //       // Create Checkout Sessions from body params.
+  //       const sessionStripe = await stripe.checkout.sessions.create({
+  //         line_items: [
+  //           {
+  //             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+  //             price: sid as string,
+  //             quantity: 1
+  //           }
+  //         ],
+  //         mode: 'subscription',
+  //         customer_email: userEmail,
+  //         success_url: `${req.headers.origin}/?success=true`,
+  //         cancel_url: `${req.headers.origin}/?canceled=true`
+  //       })
+  //       return res.redirect(303, sessionStripe.url as string)
+  //     } catch (err) {
+  //       return res.send({ message: 'error 1' })
+  //     }
+  //   } else {
+  //     res.setHeader('Allow', 'POST')
+  //     res.status(405).end('Method Not Allowed')
+  //   }
+  return res.end({ message: userEmail })
 }
