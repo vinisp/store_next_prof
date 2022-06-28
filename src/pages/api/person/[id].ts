7adffdae09d230b85a63aebd28d3f74ed3b20f07
vinisp <aragao93.vinicius@gmail.com>
2022-06-28 12:53:46 -0300

    vamos tentar pegar esse dadao

---
+++ src/pages/api/person/[id].ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getSession } from 'next-auth/react'
+import Stripe from 'stripe'
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: '2020-08-27'
+})
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  { query }: any
+) {
+  const session = await getSession({ req })
+  if (req.method === 'POST') {
+    /*
+    try {
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: 'price_1LFSVsF3qA6CuccdCsAJXlzq',
+            quantity: 1
+          }
+        ],
+        mode: 'subscription',
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`
+      })
+      return res.redirect(303, session.url)
+    } catch (err) {
+      return res.send({ message: 'error 1' })
+    }
+  */
+    console.log(session)
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method Not Allowed')
+  }
+  return res.send({ status: query.id })
+}
