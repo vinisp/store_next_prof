263028730040c7e888916ce556a02bf936f09ca9
vinisp <aragao93.vinicius@gmail.com>
2022-06-28 16:02:24 -0300

    agora vai

--- src/pages/api/sub/[sid].ts
+++ src/pages/api/sub/[sid].ts
@@ -38,5 +38,5 @@ export default async function handler(
   //     res.setHeader('Allow', 'POST')
   //     res.status(405).end('Method Not Allowed')
   //   }
-  return res.end({ message: userEmail })
+  return res.end({ message: 'userEmail' })
 }
