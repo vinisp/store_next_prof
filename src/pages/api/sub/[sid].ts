7a70d7e2985a642808545c11c8c7db2cd708fa1d
vinisp <aragao93.vinicius@gmail.com>
2022-06-28 15:43:35 -0300

    isso é ouro

--- src/pages/api/sub/[sid].ts
+++ src/pages/api/sub/[sid].ts
@@ -1,6 +1,38 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import Stripe from 'stripe'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: '2020-08-27'
+})
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { sid } = req.query
-  res.end(`Post: ${sid}`)
+
+  if (req.method === 'POST') {
+    try {
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+            price: sid as string,
+            quantity: 1
+          }
+        ],
+        mode: 'subscription',
+        success_url: `${req.headers.origin}/?success=true`,
+        cancel_url: `${req.headers.origin}/?canceled=true`
+      })
+      return res.redirect(303, session.url as string)
+    } catch (err) {
+      return res.send({ message: 'error 1' })
+    }
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method Not Allowed')
+  }
+  return res.send({ message: 'error 2' })
 }
