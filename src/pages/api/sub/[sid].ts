bfc7d17a80450367d38f55e525746ed8bb07c692
vinisp <aragao93.vinicius@gmail.com>
2022-06-28 15:31:40 -0300

    agora sei que vai

--- src/pages/api/sub/[sid].ts
+++ src/pages/api/sub/[sid].ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { pid } = req.query
-  res.end(`Post: ${pid}`)
+  const { sid } = req.query
+  res.end(`Post: ${sid}`)
 }
