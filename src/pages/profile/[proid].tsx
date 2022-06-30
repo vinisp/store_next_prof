e40d83eb030d9f010edc18c78b65183a157dd9c9
vinisp <aragao93.vinicius@gmail.com>
2022-06-30 18:47:36 -0300

    um card maneiro

--- src/pages/profile/[proid].tsx
+++ src/pages/profile/[proid].tsx
@@ -92,67 +92,68 @@ const Profile = ({ dados }: any) => {
             </div>
           </>
         ))}
-      </div>
-      <div className="col-lg-4 col-md-12 mb-4">
-        <div className={`h-100 ${styles.flip_card}`}>
-          <div className={styles.flip_card_inner}>
-            <div className={styles.flip_card_front}>
-              <span className={styles.price}>$20</span>
-              <br />
-              month
-              <br />
-              <h2 className="card-title">Standard</h2>
-              <small>Small Business</small>
-            </div>
-            <div className={styles.flip_card_back}>
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={16}
-                    height={16}
-                    fill="currentColor"
-                    className="bi bi-check"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                  </svg>{' '}
-                  Cras justo odio
-                </li>
-                <li className="list-group-item">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={16}
-                    height={16}
-                    fill="currentColor"
-                    className="bi bi-check"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                  </svg>{' '}
-                  Dapibus ac facilisis in
-                </li>
-                <li className="list-group-item">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width={16}
-                    height={16}
-                    fill="currentColor"
-                    className="bi bi-check"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                  </svg>{' '}
-                  Vestibulum at eros
-                </li>
-              </ul>
-              <button className="my-5 btn btn-outline-success btn-lg">
-                Select
-              </button>
+        <div className="col-lg-4 col-md-12 mb-4">
+          <div className={`h-100 ${styles.flip_card}`}>
+            <div className={styles.flip_card_inner}>
+              <div className={styles.flip_card_front}>
+                <span className={styles.price}>$20</span>
+                <br />
+                month
+                <br />
+                <h2 className="card-title">Standard</h2>
+                <small>Small Business</small>
+              </div>
+              <div className={styles.flip_card_back}>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={16}
+                      height={16}
+                      fill="currentColor"
+                      className="bi bi-check"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                    </svg>{' '}
+                    Cras justo odio
+                  </li>
+                  <li className="list-group-item">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={16}
+                      height={16}
+                      fill="currentColor"
+                      className="bi bi-check"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                    </svg>{' '}
+                    Dapibus ac facilisis in
+                  </li>
+                  <li className="list-group-item">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width={16}
+                      height={16}
+                      fill="currentColor"
+                      className="bi bi-check"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                    </svg>{' '}
+                    Vestibulum at eros
+                  </li>
+                </ul>
+                <button className="my-5 btn btn-outline-success btn-lg">
+                  Select
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
       <div
         className={`container ${styles.timeline} `}
         style={{ minHeight: '100vh', marginTop: '2rem' }}
