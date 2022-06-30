d76563e2482076a36569e283f7af7e80c64dbf54
vinisp <aragao93.vinicius@gmail.com>
2022-06-30 19:20:02 -0300

    fazendo o card

--- src/pages/profile/[proid].tsx
+++ src/pages/profile/[proid].tsx
@@ -5,6 +5,7 @@ import styles from './Profile.module.css'
 import { useRouter } from 'next/router'
 import { useSession } from 'next-auth/react'
 import { useEffect, useState } from 'react'
+import { hide } from '@popperjs/core'
 
 const Profile = ({ dados }: any) => {
   const PostsToRender = dados.map((e: any) => ({
@@ -77,81 +78,29 @@ const Profile = ({ dados }: any) => {
         style={{
           border: '1px solid black',
           display: 'flex',
+          justifyContent: 'center',
           flexWrap: 'wrap',
           padding: '1rem'
         }}
       >
         {plansPrices.map((e: any) => (
           <>
-            <div style={{ border: '1px solid red', borderRadius: '8px' }}>
+            <div
+              style={{
+                border: '1px solid red',
+                borderRadius: '8px',
+                width: 'min(350px, 80vw)',
+                height: '400px'
+              }}
+            >
               <form action={`/api/sub/${e.priceid}/${userEmail}`} method="POST">
                 <h1>{e.planName}</h1>
-                <h3>{e.price}</h3>
-                <button>{e.planName}</button>
+                <h3>R$ {+e.price / 100}</h3>
+                <button className="btn btn-success">{e.planName}</button>
               </form>
             </div>
           </>
         ))}
-        <div className="col-lg-4 col-md-12 mb-4">
-          <div className={`h-100 ${styles.flip_card}`}>
-            <div className={styles.flip_card_inner}>
-              <div className={styles.flip_card_front}>
-                <span className={styles.price}>$20</span>
-                <br />
-                month
-                <br />
-                <h2 className="card-title">Standard</h2>
-                <small>Small Business</small>
-              </div>
-              <div className={styles.flip_card_back}>
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={16}
-                      height={16}
-                      fill="currentColor"
-                      className="bi bi-check"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                    </svg>{' '}
-                    Cras justo odio
-                  </li>
-                  <li className="list-group-item">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={16}
-                      height={16}
-                      fill="currentColor"
-                      className="bi bi-check"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                    </svg>{' '}
-                    Dapibus ac facilisis in
-                  </li>
-                  <li className="list-group-item">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={16}
-                      height={16}
-                      fill="currentColor"
-                      className="bi bi-check"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-                    </svg>{' '}
-                    Vestibulum at eros
-                  </li>
-                </ul>
-                <button className="my-5 btn btn-outline-success btn-lg">
-                  Select
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
 
       <div
