320c70f7fad5b922fd824b99c6349bcdf5521084
vinisp <aragao93.vinicius@gmail.com>
2022-07-05 12:12:40 -0300

    o que vai dar isso

--- src/pages/privatepost/[pid].tsx
+++ src/pages/privatepost/[pid].tsx
@@ -75,6 +75,59 @@ const Privatepost = () => {
         }}
       >
         <div>
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col col-lg-9 col-xl-7">
+              <div className="card">
+                <div
+                  className="rounded-top text-white d-flex flex-row"
+                  style={{ backgroundColor: '#000', height: '200px' }}
+                >
+                  <div
+                    className="ms-4 mt-5 d-flex flex-column"
+                    style={{ width: '150px' }}
+                  >
+                    <img
+                      src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                      alt="Generic placeholder image"
+                      className="img-fluid img-thumbnail mt-4 mb-2"
+                      style={{ width: '150px', zIndex: 1 }}
+                    />
+                    <button
+                      type="button"
+                      className="btn btn-outline-dark"
+                      data-mdb-ripple-color="dark"
+                      style={{ zIndex: 1 }}
+                    >
+                      Edit profile
+                    </button>
+                  </div>
+                  <div className="ms-3" style={{ marginTop: '130px' }}>
+                    <h5>Andy Horwitz</h5>
+                    <p>New York</p>
+                  </div>
+                </div>
+                <div
+                  className="p-4 text-black"
+                  style={{ backgroundColor: '#f8f9fa' }}
+                >
+                  <div className="d-flex justify-content-end text-center py-1">
+                    <div>
+                      <p className="mb-1 h5">253</p>
+                      <p className="small text-muted mb-0">Photos</p>
+                    </div>
+                    <div className="px-3">
+                      <p className="mb-1 h5">1026</p>
+                      <p className="small text-muted mb-0">Followers</p>
+                    </div>
+                    <div>
+                      <p className="mb-1 h5">478</p>
+                      <p className="small text-muted mb-0">Following</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
           <Rating
             name="simple-controlled"
             value={value}
